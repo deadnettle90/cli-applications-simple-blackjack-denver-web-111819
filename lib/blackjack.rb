9283aa034f476e1d70437c7dc4a6b841cc0436e7
Danielle Matton <danimatton@gmail.com>
2019-11-11 22:24:06 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -42,9 +42,9 @@ def hit?(card_total)
   # code hit? here
   prompt_user
   get_user_input
-  if get_user_input = "h"
+  if get_user_input == "h"
     card_total += deal_card
-    elsif get_user_input = "s"
+    elsif get_user_input == "s"
     card_total
   else
     invalid_command
