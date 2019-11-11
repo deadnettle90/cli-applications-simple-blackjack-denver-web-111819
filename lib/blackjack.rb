2ba8a33778060ceecf4806d1f452510ffa72c4f8
Danielle Matton <danimatton@gmail.com>
2019-11-11 22:59:29 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -64,8 +64,7 @@ end
 
 def runner
   welcome
-  count = initial_round
-  card_total = count
+  card_total = initial_round
   until card_total > 21
     card_total = hit?(card_total)
     display_card_total(card_total)
